0d2d25ea1b1e5def085c1458c7f3c26c379268af
zhangke <zhangke>
2018-08-02 18:20:50 +0800

    main

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"grpc-go-stream/server"
 	"grpc-go-stream/client"
+	"time"
+	"grpc-go-stream/server"
 )
 
 func main(){
-	server.Run()
+	go func() {
+		server.Run()
+	}()
+	time.Sleep(3*time.Millisecond)
 	client.Run()
 }
